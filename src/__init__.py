6234f804751a4a6211c9170be9b14691dff33527
Nisarg Upadhyaya <nisarg1631@gmail.com>
2023-01-25 15:01:49 +0530

    add views

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from src.models import MasterQueue
+from src.json_validator import expects_json
 
 app = Flask(__name__)
+master_queue = MasterQueue()
 
 from src import views
--- src/models/topic.py
+++ src/models/topic.py
@@ -1,11 +1,18 @@
 import threading
 
-from src.datastructures import ThreadSafeLogQueue, ThreadSafeConsumerDict, ThreadSafeProducerSet
+from src.datastructures import (
+    ThreadSafeLogQueue,
+    ThreadSafeConsumerDict,
+    ThreadSafeProducerSet,
+)
+from src.models import Log
 
-class Topic():
+
+class Topic:
     """
     A topic is a collection of log messages that are related to each other.
     """
+
     def __init__(self, name: str):
         self._name = name
         self._logs = ThreadSafeLogQueue()
@@ -15,11 +22,37 @@ class Topic():
     def get_length(self) -> int:
         """Return the length of the topic."""
         return len(self._logs)
-    
+
     def get_log(self, index: int) -> Log:
         """Return the log at the given index."""
         return self._logs[index]
-    
+
     def add_log(self, log: Log) -> None:
         """Add a log to the topic."""
         self._logs.append(log)
+
+    def add_producer(self, producer_id: str) -> None:
+        """Add a producer to the topic."""
+        self._producers.add(producer_id)
+
+    def add_consumer(self, consumer_id: str) -> None:
+        """Add a consumer to the topic."""
+        self._consumers.add(consumer_id)
+
+    def check_producer(self, producer_id: str) -> bool:
+        """Return whether the producer is in the topic."""
+        return self._producers.contains(producer_id)
+
+    def check_consumer(self, consumer_id: str) -> bool:
+        """Return whether the consumer is in the topic."""
+        return self._consumers.contains(consumer_id)
+
+    def get_consumer_offset(self, consumer_id: str) -> int:
+        """Return the consumer offset."""
+        return self._consumers.get_offset(consumer_id)
+
+    def get_and_increment_consumer_offset(
+        self, consumer_id: str, threshold: int
+    ) -> int:
+        """Return the consumer offset and increment it by one."""
+        return self._consumers.get_offset_and_increment(consumer_id, threshold)
